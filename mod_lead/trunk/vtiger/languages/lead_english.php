87d2f7019eeb9f0b80f28f3bbd82d717d110ab3f
Matthew Brichacek <mmbrich@users.noreply.github.com>
2006-06-19 22:28:33 +0000

    Added Pierre's changes: add multilingual text file with detetection of the language move the server parameter to the mambot check if the mambot are installed display a message after the user press "Submit"

---
+++ mod_lead/trunk/vtiger/languages/lead_english.php
@@ -0,0 +1,19 @@
+<?php 
+/**
+* @version 1.1 $
+* @package VtigerLead
+* @copyright (C) 2006 Pierre-André Vullioud www.paimages.ch            
+* @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
+*/
+
+DEFINE('_LAST_NAME','Last Name');
+DEFINE('_PHONE','Phone');
+DEFINE('_COMPANY_NAME','Company Name');
+DEFINE('_COUNTRY','Country');
+DEFINE('_DESCRIPTION','Description');
+
+DEFINE('_SUCCESS','Your message has been send');
+
+#error message
+DEFINE('_INSTALL_BOT','You should install and publish the vconnection mambot first');
+?>
