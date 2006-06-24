36bbd7cc72d89dde8fd4fee9a2781d5377c1a22d
Matthew Brichacek <mmbrich@users.noreply.github.com>
2006-06-24 20:00:27 +0000

    added new mod_lead for vtiger5 changed to new webforms.php SOAP server added assigned_user_id to array

---
+++ vtiger5/trunk/mod_lead/vtiger/languages/lead_french.php
@@ -0,0 +1,18 @@
+<?php 
+/**
+* @version 1.1 $
+* @package VtigerLead
+* @copyright (C) 2006 Pierre-André Vullioud www.paimages.ch            
+* @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
+*/
+
+DEFINE('_LAST_NAME','Nom');
+DEFINE('_PHONE','Téléphone');
+DEFINE('_COMPANY_NAME','Société');
+DEFINE('_COUNTRY','Pays');
+DEFINE('_DESCRIPTION','Votre message');
+DEFINE('_SUCCESS','Votre demande a été envoyée');
+
+#error message
+DEFINE('_INSTALL_BOT','Vous devez installer et publier le mambot vconnection pour pouvoir utiliser ce module');
+?>
