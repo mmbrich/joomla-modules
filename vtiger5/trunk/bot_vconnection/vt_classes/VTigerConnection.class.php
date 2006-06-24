0663f4425ab1a6b8b17e7866d8ba9f4bdab66c76
Matthew Brichacek <mmbrich@users.noreply.github.com>
2006-06-24 19:59:14 +0000

    new vconnection bot for vtiger5.x Fixed soapclient re-declaration Fixed typo in soapserver variable

---
+++ vtiger5/trunk/bot_vconnection/vt_classes/VTigerConnection.class.php
@@ -0,0 +1,50 @@
+<?php
+/*
+ * The contents of this file are subject to the vtiger CRM Public License 
+ * Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ *
+ * Portions created by Matthew Brichacek are Copyright (C) 2005 netXccel.
+ *
+ * All Rights Reserved.
+ *
+ * Contributors: _________________
+ *
+ */
+require_once("nusoap/lib/nusoap.php");
+class VTigerConnection
+{
+	var $client;
+	var $result;
+	var $data;
+	var $command;
+	var $server;
+
+	function VTigerConnection($file)
+	{
+		global $database;
+		// load bot_vconnection mambot parameters
+  		$query = "SELECT id FROM #__mambots WHERE element = 'bot_vconnection' AND folder = 'system'";
+  		$database->setQuery( $query );
+  		$id = $database->loadResult();
+  		$mambot = new mosMambot( $database );
+  		$mambot->load( $id );
+  		$mambotParams =& new mosParameters( $mambot->params );
+  		$vtiger_lead_soapserver = $mambotParams->get( 'vtiger_lead_soapserver', 'basic' );
+  
+		$this->client = new soapclient2($vtiger_lead_soapserver."/".$file);
+	}
+	function setData($data)
+	{
+		$this->data = $data;
+	}
+	function execCommand($command)
+	{
+		$this->result = $this->client->call($command, $this->data);
+		if(($this->client->getError()) || ($this->result == "failed"))
+			return "failed";
+		else
+			return $this->result;
+	}
+}
+?>
