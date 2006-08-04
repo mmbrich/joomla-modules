62c99176c942e7f75602439d957b438b10a94a82
mattb <mattb@9c9e6dd4-b40c-0410-86a4-91b7dd187b41>
2006-08-04 19:17:52 +0000

    added functions for vfield built-in value populator to keep SOAP connections to a minimum

--- vtiger5/trunk/soap/fields.php
+++ vtiger5/trunk/soap/fields.php
@@ -49,8 +49,20 @@ $server->wsdl->addComplexType(
                 'uitype' => array('name'=>'uitype','type'=>'xsd:string'),
                 'fieldname' => array('name'=>'fieldname','type'=>'xsd:string'),
                 'fieldlabel' => array('name'=>'fieldlabel','type'=>'xsd:string'),
-                'maximumlength' => array('name'=>'fieldlabel','type'=>'xsd:string'),
-                'value' => array('name'=>'fieldlabel','type'=>'xsd:string')
+                'maximumlength' => array('name'=>'maximumlength','type'=>'xsd:string'),
+                'value' => array('name'=>'value','type'=>'xsd:string'),
+                'values' => array('name'=>'values','type'=>'xsd:string')
+        )
+);
+
+$server->wsdl->addComplexType(
+        'save_field_type',
+        'complexType',
+        'array',
+        '',
+        array(
+                'columnname' => array('name'=>'columnname','type'=>'xsd:string'),
+                'value' => array('name'=>'value','type'=>'xsd:string')
         )
 );
 
@@ -81,45 +93,168 @@ $server->register(
 	$NAMESPACE
 );
 
+$server->register(
+	'save_form_fields',
+	array(
+		'entityid'=>'xsd:string',
+		'module'=>'xsd:string',
+		'fields'=>'tns:save_field_type'
+	),
+	array(
+		'return'=>'xsd:string'
+	),
+	$NAMESPACE
+);
+
+function save_form_fields($entityid,$module,$fields) {
+	global $adb,$current_user;
+	$adb->println("Enter into the function save_form_fields($entityid,$module,$fields)");
+
+	if($entityid != "") {
+		$q = "SELECT smownerid FROM vtiger_crmentity WHERE crmid='".$entityid."'";
+		$rs = $adb->query($q);
+		$current_owner = $adb->query_result($rs,'0','smownerid');
+		if($current_owner == "" || !isset($current_owner)) $current_owner=1;
+
+        	require_once('modules/Users/User.php');
+        	$current_user = new User();
+        	$current_user->retrieve_entity_info($current_owner,"Users");
+	}
+
+	if($module == "Products") {
+		require_once('modules/Products/Product.php');
+		$focus = new Product();
+	} else if($module == "Contacts") {
+		require_once('modules/Contacts/Contact.php');
+		$focus = new Contact();
+	} else if($module == "Accounts") {
+		require_once('modules/Accounts/Account.php');
+		$focus = new Account();
+	} else if($module == "Leads") {
+		require_once('modules/Leads/Lead.php');
+		$focus = new Lead();
+	} else if($module == "Activities") {
+		require_once('modules/Activities/Activity.php');
+		$focus = new Activity();
+	} else if($module == "Campaigns") {
+		require_once('modules/Campaigns/Campaign.php');
+		$focus = new Campaign();
+	} else if($module == "HelpDesk") {
+		require_once('modules/HelpDesk/HelpDesk.php');
+		$focus = new HelpDesk();
+	} else if($module == "Invoice") {
+		require_once('modules/Invoice/Invoice.php');
+		$focus = new Invoice();
+	} else if($module == "Potentials") {
+		require_once('modules/Potentials/Potential.php');
+		$focus = new Potential();
+	} else if($module == "Quotes") {
+		require_once('modules/Quotes/Quote.php');
+		$focus = new Quote();
+	} else if($module == "SalesOrder") {
+		require_once('modules/SalesOrder/SalesOrder.php');
+		$focus = new SalesOrder();
+	}
+	if($entityid != "" && $entityid != 0) {
+		$focus->retrieve_entity_info($entityid,$module);
+		$focus->id = $entityid;
+		$focus->mode = "edit";
+	}
+
+	for($j=0;$j<count($fields);$j++) {
+		$adb->println("\n\rSaving\n\r");
+		$focus->column_fields[$fields[$j]["columnname"]] = $fields[$j]["value"];
+	}
+	$focus->save($module);
+
+	return $focus->id;
+}
+
 function get_field_details($module,$columnname,$entityid) {
 	global $adb;
 	$adb->println("Enter into the function get_field_details($module,$columnname)");
 
 	$tabid=getTabid($module);
-
-	$q = "SELECT * FROM vtiger_field "
-		." WHERE columnname='".$columnname."' "
-		." AND tabid='".$tabid."'";
+	$q = "SELECT fieldid,columnname,uitype,fieldname,fieldlabel,maximumlength FROM vtiger_field ";
+	if($columnname != "") {
+		$q .= " WHERE columnname='".$columnname."' ";
+		$q .= " AND tabid='".$tabid."'";
+	} else {
+		$q .= " WHERE tabid='".$tabid."'";
+	}
 	$rs = $adb->query($q);
 
-        $field[0]["fieldid"] = $adb->query_result($rs,'0','fieldid');
-        $field[0]["columnname"] = $adb->query_result($rs,'0','columnname');
-        $field[0]["uitype"] = $adb->query_result($rs,'0','uitype');
-        $field[0]["fieldname"] = $adb->query_result($rs,'0','fieldname');
-        $field[0]["fieldlabel"] = $adb->query_result($rs,'0','fieldlabel');
-        $field[0]["maximumlength"] = $adb->query_result($rs,'0','maximumlength');
-	if($entityid != "")
-        	$field[0]["value"] = get_field_values($columnname,$adb->query_result($rs,'0','tablename'),$entityid);
-	else
-        	$field[0]["value"] = "";
+	for($i=0,$num=$adb->num_rows($rs);$i<$num;$i++) {
+        	$field[$i]["fieldid"] = $adb->query_result($rs,$i,'fieldid');
+        	$field[$i]["columnname"] = $adb->query_result($rs,$i,'columnname');
+        	$field[$i]["uitype"] = $adb->query_result($rs,$i,'uitype');
+        	$field[$i]["fieldname"] = $adb->query_result($rs,$i,'fieldname');
+        	$field[$i]["fieldlabel"] = $adb->query_result($rs,$i,'fieldlabel');
+        	$field[$i]["maximumlength"] = $adb->query_result($rs,$i,'maximumlength');
 
+		// Populate the picklist values and field values where needed
+		if($entityid != "") {
+			if($field[$i]["uitype"] == "15" || $field[$i]["uitype"] == "33") {
+        			$field[$i]["values"] = get_picklist_values($field[$i]["fieldid"]);
+        			$field[$i]["value"] = get_field_values($columnname,$entityid,$module);
+			} else {
+        			$field[$i]["value"] = get_field_values($columnname,$entityid,$module);
+        			$field[$i]["values"] = "";
+			}
+		} else {
+        		$field[$i]["value"] = "";
+        		$field[$i]["values"] = "";
+			if($field[$i]["uitype"] == "15" || $field[$i]["uitype"] == "33")
+        			$field[$i]["values"] = get_picklist_values($field[$i]["fieldid"]);
+		}
+	}
 	return $field;
 }
 
-function get_field_values($columnname,$tablename,$entityid) {
+function get_field_values($columnname,$entityid,$module) {
+	if($entityid == "" || !isset($entityid))
+		return '';
 	global $adb;
+	$adb->println("Enter into the function get_field_values($columnname,$entityid,$module)");
 
-	$q = "SELECT ".$columnname." FROM ".$tablename." "
-		." INNER JOIN vtiger_crmentity ON vtiger_crmentity.crmid=".$entityid;
-	$rs = $adb->query($q);
-
-	return $adb->query_result($rs,'0',$columnname);
+	if($module == "Products") {
+		require_once('modules/Products/Product.php');
+		$focus = new Product();
+	} else if($module == "Contacts") {
+		require_once('modules/Contacts/Contact.php');
+		$focus = new Contact();
+	} else if($module == "Accounts") {
+		require_once('modules/Accounts/Account.php');
+		$focus = new Account();
+	} else if($module == "Leads") {
+		require_once('modules/Leads/Lead.php');
+		$focus = new Lead();
+	} else if($module == "Activities") {
+		require_once('modules/Activities/Activity.php');
+		$focus = new Activity();
+	} else if($module == "Campaigns") {
+		require_once('modules/Campaigns/Campaign.php');
+		$focus = new Campaign();
+	} else if($module == "HelpDesk") {
+		require_once('modules/HelpDesk/HelpDesk.php');
+		$focus = new HelpDesk();
+	} else if($module == "Invoice") {
+		require_once('modules/Invoice/Invoice.php');
+		$focus = new Invoice();
+	} else if($module == "Potentials") {
+		require_once('modules/Potentials/Potential.php');
+		$focus = new Potential();
+	} else if($module == "Quotes") {
+		require_once('modules/Quotes/Quote.php');
+		$focus = new Quote();
+	} else if($module == "SalesOrder") {
+		require_once('modules/SalesOrder/SalesOrder.php');
+		$focus = new SalesOrder();
+	}
+	$focus->retrieve_entity_info($entityid,$module);
+	return $focus->column_fields[$columnname];
 }
 
-/**	function to list available fields
- *	@param string $emailid - email address to unsubscribe
- *	return message about the success or failure status about the unsubscribe
- */
 function get_portal_register_fields($module_name)
 {
 	global $adb;
@@ -145,7 +280,7 @@ function get_portal_register_fields($module_name)
 
 function get_picklist_values($fieldid) {
 	global $adb;
-	$adb->println("Enter into the function unsubscribe_email($emailid)");
+	$adb->println("Enter into the function get_picklist_values($fieldid)");
 	
 	$rs = $adb->query("SELECT fieldname FROM vtiger_field WHERE fieldid='".$fieldid."'");
 	$fieldname = $adb->query_result($rs,'0','fieldname');
