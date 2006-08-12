943af89c41b2fe94e49f10d798d1a73308fc201f
Matthew Brichacek <mmbrich@users.noreply.github.com>
2006-08-12 18:52:51 +0000

    more feature abilities in salesorder, can now populate and re-calculate an SO, also converts to invoice on checkout

--- vtiger5/trunk/mod_salesorder/mod_salesorder.php
+++ vtiger5/trunk/mod_salesorder/mod_salesorder.php
@@ -9,18 +9,38 @@
  
 /** ensure this file is being included by a parent file */
 defined( '_VALID_MOS' ) or die( 'Direct Access to this location is not allowed.' );
- 
-#check if the bot exist
-if (!file_exists($mosConfig_absolute_path.'/mambots/system/vt_classes/VTigerConnection.class.php'))
-	echo "Install BOT_VCONNECTION";exit();
+
+// check if the bot exist
+if (!file_exists($mosConfig_absolute_path.'/mambots/system/vt_classes/VTigerConnection.class.php')) {
+        echo "You should install bot_vconnection if you want something to happen here ;)";
+        flush();exit();
+}
 
 global $my;
+require_once($mosConfig_absolute_path.'/components/com_vtigerregistration/vtiger/VTigerSalesOrder.class.php');
+$SalesOrder = new VtigerSalesOrder();
 
-//require_once($mosConfig_absolute_path.'/components/com_vtigerregistration/vtiger/VtigerSalesorder.class.php');
+if(!$my->id) {
+	if(!isset($_COOKIE["current_salesorder"]))
+		echo "<p style='text-align:center;font-weight:bold'>You have no items in your cart</p>";
+	else
+		echo "<p style='text-align:center;font-weight:bold'>Please log in to complete your transaction</p>";
+} else {
+	if($my->id && $_COOKIE["current_salesorder"]) {
+		if($SalesOrder->Checkid($my->id)) {
+			$soid = $SalesOrder->AssociateToUser();
+		}
+	}
+	echo "<div style='valign:top;text-align:center;font-weight:bold'>Cart for ".$my->name."</div>";
 
-if(!$my->id)
-	echo "Welcome to sales order";
-else
-	echo "Hello ".$my->name
-
+	$tmp = $SalesOrder->GetCurrentSalesOrders($my->id);
+	if(is_array($tmp)) {
+		echo "<br><table border='0' cellspacing='0' cellpadding='0' style='text-align:center' align='center'>";
+		echo "<tr><th>Name</th><th>&nbsp;</th><th>Total</th></tr>";
+		foreach($tmp as $order) {
+			echo "<tr><td><a href='index.php?option=com_vtigersalesorders&task=view&soid=".$order["salesorderid"]."'>".$order["subject"]."</a></td><td>&nbsp;</td><td>$".$order["total"]."</td></tr>";
+		}
+		echo "</table>";
+	}
+}
 ?>
