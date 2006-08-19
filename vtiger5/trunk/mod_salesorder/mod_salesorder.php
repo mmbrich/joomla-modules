fa9d36032b1bc37173d91603a32b49b9e10b6fc5
mattb <mattb@9c9e6dd4-b40c-0410-86a4-91b7dd187b41>
2006-08-19 19:45:42 +0000

    cleaned up UI and cookie handling

--- vtiger5/trunk/mod_salesorder/mod_salesorder.php
+++ vtiger5/trunk/mod_salesorder/mod_salesorder.php
@@ -15,7 +15,6 @@ if (!file_exists($mosConfig_absolute_path.'/mambots/system/vt_classes/VTigerConn
         echo "You should install bot_vconnection if you want something to happen here ;)";
         flush();exit();
 }
-
 global $my;
 require_once($mosConfig_absolute_path.'/components/com_vtigerregistration/vtiger/VTigerSalesOrder.class.php');
 $SalesOrder = new VtigerSalesOrder();
@@ -23,12 +22,37 @@ $SalesOrder = new VtigerSalesOrder();
 if(!$my->id) {
 	if(!isset($_COOKIE["current_salesorder"]))
 		echo "<p style='text-align:center;font-weight:bold'>You have no items in your cart</p>";
-	else
-		echo "<p style='text-align:center;font-weight:bold'>Please log in to complete your transaction</p>";
+	else {
+		$tmp = $SalesOrder->GetCurrentSalesOrders();
+		if(is_array($tmp)) {
+			?>
+			<div style='valign:top;text-align:center;font-weight:bold'>Please log-in.</div>
+			<br>
+			<table border='0' cellspacing='0' cellpadding='0' style='text-align:center' align='center'>
+				<tr>
+					<td colspan='3'>
+						<a href='index.php?option=com_vtigersalesorders&task=view&soid=<?php echo $tmp[0]["salesorderid"];?>'>
+							You have <?php echo $tmp[0]["num_products"];?> products in your cart.
+						</a>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						$<?php echo $tmp[0]["total"];?>
+					</td>
+				</tr>
+			</table>
+			<?
+		} else {
+			echo "<p style='text-align:center;font-weight:bold'>You cart is empty.</p>";
+			setcookie("current_salesorder", "", time()-3600);
+		}
+	}
 } else {
 	if($my->id && $_COOKIE["current_salesorder"]) {
 		if($SalesOrder->Checkid($my->id)) {
 			$soid = $SalesOrder->AssociateToUser();
+			mosRedirect('index.php');
 		}
 	}
 	echo "<div style='valign:top;text-align:center;font-weight:bold'>Cart for ".$my->name."</div>";
@@ -36,9 +60,22 @@ if(!$my->id) {
 	$tmp = $SalesOrder->GetCurrentSalesOrders($my->id);
 	if(is_array($tmp)) {
 		echo "<br><table border='0' cellspacing='0' cellpadding='0' style='text-align:center' align='center'>";
-		echo "<tr><th>Name</th><th>&nbsp;</th><th>Total</th></tr>";
+
 		foreach($tmp as $order) {
-			echo "<tr><td><a href='index.php?option=com_vtigersalesorders&task=view&soid=".$order["salesorderid"]."'>".$order["subject"]."</a></td><td>&nbsp;</td><td>$".$order["total"]."</td></tr>";
+		?>
+			<tr>
+				<td colspan='3'>
+					<a href='index.php?option=com_vtigersalesorders&task=view&soid=<?php echo $order["salesorderid"];?>'>
+						You have <?php echo $order["num_products"];?> products in your cart.
+					</a>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					$<?php echo $order["total"];?>
+				</td>
+			</tr>
+		<?
 		}
 		echo "</table>";
 	}
