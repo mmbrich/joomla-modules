be5bdcd7014adfe02d80f0bcb31029b602df491d
Matthew Brichacek <mmbrich@users.noreply.github.com>
2006-08-28 03:13:48 +0000

    internationalization for products

--- vtiger5/trunk/com_vtigerproducts/vtigerproducts.php
+++ vtiger5/trunk/com_vtigerproducts/vtigerproducts.php
@@ -2,13 +2,20 @@
 // no direct access
 defined('_VALID_MOS') or die('Restricted access');
 
-#check if the bot exist
+// check if the bot exist
 if (file_exists($mosConfig_absolute_path.'/mambots/system/vt_classes/VTigerConnection.class.php')) {
 	require_once('components/com_vtigerregistration/vtiger/VTigerProduct.class.php');
 	$vProduct = new VtigerProduct();
 } else {
 	echo "You should install bot_vconnection if you want something to happen here ;)";
 	flush();exit();
+}
+
+// Get the right language if it exists
+if (file_exists($mosConfig_absolute_path.'/components/com_vtigerproducts/languages/vtigerproducts_'.$mosConfig_lang.'.php')) {
+    include($mosConfig_absolute_path.'/components/com_vtigerproducts/languages/vtigerproducts_'.$mosConfig_lang.'.php');
+} else {
+    include($mosConfig_absolute_path.'/components/com_vtigerproducts/languages/vtigerproducts_english.php');
 }
 require_once( $mainframe->getPath( 'front_html' ) );
 
@@ -36,7 +43,6 @@ switch($task) {
 		$limit_start = mosGetParam( $_REQUEST, 'limitstart' , '0');
         	require_once( $GLOBALS['mosConfig_absolute_path'] . '/includes/pageNavigation.php' );
         	$pageNav = new mosPageNav( count($list), $limit_start, $limit );
-
 		HTML_product::listProducts( $option, $list, $category, $conf, $limit, $limit_start, $pageNav );
 	break;
 }
